adfe48b83dc854bc79230fea16dc6ac197a64a9c
vishnuGuptha <vishnugupta28899@gmail.com>
2023-11-05 16:24:19 +0530

    projects updated

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,10 +4,11 @@ import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 // import leaf from "../../Assets/Projects/leaf.png";
 // import emotion from "../../Assets/Projects/emotion.png";
-// import editor from "../../Assets/Projects/codeEditor.png";
+import News from "../../Assets/Projects/News.png";
+import Qkart from "../../Assets/Projects/Qkart.png";
 import chatify from "../../Assets/Projects/chatify.png";
-// import suicide from "../../Assets/Projects/suicide.png";
-// import bitsOfCode from "../../Assets/Projects/blog.png";
+import QTripDynamic from "../../Assets/Projects/QTripDynamic.png";
+import QTripStatic from "../../Assets/Projects/QTripStatic.jpg";
 
 function Projects() {
   return (
@@ -23,70 +24,58 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
+              imgPath={Qkart}
+              isBlog={false}
+              title="Qkart"
+              description="QKart is an e-commerce application offering a variety of products for customers to choose from. During the course of this project, Implemented the core logic for authentication, shopping cart and checkout Improved UI by adding responsive design elements for uniform experience across different devices Utilized REST APIs to dynamically load and render data served by the backend server"
+              ghLink="https://github.com/vishnuGuptha/vishnugupta28899-ME_QKART_FRONTEND_V2"
+              demoLink="https://shopping-kart01.netlify.app/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
               imgPath={chatify}
               isBlog={false}
-              title="Chatify"
+              title="Weather App"
               description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
               ghLink="https://github.com/vishnuGuptha/Responsive-Card-Layout"
               demoLink="https://master--vishnu-weather.netlify.app/"
             />
           </Col>
 
-          {/* <Col md={4} className="project-card">
+          <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={QTripDynamic}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              title="QTrip Dynamic"
+              description="QTrip is a travel website aimed at travellers looking for a multitude of adventures in different cities. During the course of this project,Created web pages using HTML and CSS and made them dynamic using JavaScript Improved UX with multi-select filters, image carousels Utilised localStorage to persist user preferences at client-side"
+              ghLink="https://github.com/vishnuGuptha/vishnugupta28899-ME_QTRIPDYNAMIC"
+              demoLink="https://qtripvishnu.netlify.app/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={QTripStatic}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"
+              title="QTrip Static"
+              description="QTrip is a travel website aimed at travellers looking for a multitude of adventures in different cities. During the course of this project, Created 3 different web pages from Wireframe layout using HTML and CSS Utilized Bootstrap extensively for responsive design Deployed the website to Netlify/Vercel"
+              ghLink="https://github.com/vishnuGuptha/vishnugupta28899-ME_QTRIPDYNAMIC"
+              demoLink="https://triptrekker.netlify.app/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={leaf}
+              imgPath={News}
               isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
+              title="XBoard"
+              description="XBoard is a News Feed website that will feature the latest news for select topics, from Flipboard. During the course of this project, Built XBoard using HTML, CSS, Bootstrap and JavaScript from scratch Used Accordions and Image Carousel to improve UI Fetched news content from flipboard's RSS feed using REST API"
+              ghLink="https://github.com/vishnuGuptha/vishnugupta28899-ME_QTRIPDYNAMIC"
+              demoLink="https://breaking-news-vishnu.netlify.app/"
             />
           </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={suicide}
-              isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={emotion}
-              isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here
-            />
-          </Col> */}
         </Row>
       </Container>
     </Container>
